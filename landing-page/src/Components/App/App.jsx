f01803d13fb8ff16c2f45c78e7dc6debc07d659f
Nikita <novakhudonosor@mail.ru>
2022-10-03 10:21:32 +0300

    Commit fix

---
+++ landing-page/src/Components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import InfBlock from "../InfBlock";
+import Content from "../Content";
+
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <InfBlock/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
