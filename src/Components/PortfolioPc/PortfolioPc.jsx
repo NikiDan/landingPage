8e7287289b1b9ba508118c0d18233c4801dfca69
Nikita <novakhudonosor@mail.ru>
2022-12-29 11:34:51 +0300

    Cards edited

--- src/Components/PortfolioPc/PortfolioPc.jsx
+++ src/Components/PortfolioPc/PortfolioPc.jsx
@@ -5,7 +5,7 @@ import imgSquare from "../img/Square.png";
 import imgTodo from "../img/Todo.png";
 
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+import { CardActionArea } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
@@ -13,8 +13,30 @@ import "./style.css";
 import {Typography} from "@mui/material";
 
 const PortfolioPc = () => {
+
+    const card = [
+        {
+            key: "neirika",
+            link: "https://nikidan.github.io/Neirika/index.html",
+            title: "Neirika homepage",
+            img: imgNeirika
+        },
+        {
+            key: "cvetOzera",
+            link: "https://nikidan.github.io/CvetOzera/index.html",
+            title: "Store",
+            img: imgSquare
+        },
+        {
+            key: "todo",
+            link: "https://heartfelt-bunny-71754e.netlify.app/",
+            title: "Todo list",
+            img: imgTodo
+        },
+    ]
+
     return(
-        <div className="content portfolio__content__main">
+        <div className="portfolio__content__main">
             <h1 className="portfolio__header__text">My projects</h1>
             <div className="nav-container">
                 <ul className="nav">
@@ -30,72 +52,27 @@ const PortfolioPc = () => {
                 </ul>
             </div>
             <div className="portfolio__content">
-                <Card className="portfolio__item neirika portfolio__card">
-                    <CardMedia className="card__img-container"
-                               component="img"
-                               alt="Neirika homepage"
-                               height="140"
-                               image={imgNeirika}
-                    />
-                    <CardContent >
-                        <Typography component="p" className="card__description">
-                            Neirika homepage
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                    <a href="https://nikidan.github.io/Neirika/index.html"
-                       target="_blank"
-                       className="portfolio__card-link"
-                       rel="noopener noreferrer"
-                    >
-                        View
-                    </a>
-                    </CardActions>
-                </Card>
-                <Card className="portfolio__item square portfolio__card">
-                    <CardMedia className="card__img-container"
-                               component="img"
-                               alt="Store"
-                               height="140"
-                               image={imgSquare}
-                    />
-                    <CardContent>
-                        <Typography component="p" className="card__description">
-                            Store
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                    <a href="https://nikidan.github.io/CvetOzera/index.html"
-                       target="_blank"
-                       className="portfolio__card-link"
-                       rel="noopener noreferrer"
-                    >
-                        View
-                    </a>
-                    </CardActions>
-                </Card>
-                <Card className="portfolio__item todo portfolio__card">
-                    <CardMedia className="card__img-container"
-                               component="img"
-                               alt="Todo list"
-                               height="140"
-                               image={imgTodo}
-                    />
-                    <CardContent>
-                        <Typography component="p" className="card__description">
-                            Some text
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <a href="https://heartfelt-bunny-71754e.netlify.app/"
-                           target="_blank"
-                           className="portfolio__card-link"
-                           rel="noopener noreferrer"
+                {card.map((item) => (
+                    <Card className="portfolio__item portfolio__card" key={item.key} id={item.key}>
+                        <CardActionArea href={item.link}
+                                        target="_blank"
+                                        className="portfolio__card-link"
+                                        rel="noopener noreferrer"
                         >
-                            View
-                        </a>
-                    </CardActions>
-                </Card>
+                            <CardMedia className="card__img-container"
+                                       component="img"
+                                       alt="Neirika homepage"
+                                       height="140"
+                                       image={item.img}
+                            />
+                            <CardContent >
+                                <Typography component="p" className="card__description">
+                                    {item.title}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                ))}
             </div>
         </div>
     );
